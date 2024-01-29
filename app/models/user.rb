e3d778f4073830df1a59580a85aef3dab1edaad1
Roman Serediuk <box4rom@gmail.com>
2024-01-29 12:02:17 +0200

    add PROFILE_TITLE data

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  PROFILE_TITLE = [
+    'Senior Ruby on Rails Developer',
+    'Full Stack Ruby on Rails Developer',
+    'Senior Full Stack Ruby on Rails Developer',
+    'Junior Full Stack Ruby on Rails Developer',
+    'Senior Javascript Developer',
+    'Senior Frontend Developer',
+].freeze
+
+
 end
