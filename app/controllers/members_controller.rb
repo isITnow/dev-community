455af0bf984660cb6e9f5a17bbb312cfa8c17de4
Roman Serediuk <box4rom@gmail.com>
2024-02-01 12:02:01 +0200

    feat: update personal details

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,5 +1,5 @@
 class MembersController < ApplicationController
-  before_action :authenticate_user!, only: %i[edit_description update_description]
+  before_action :authenticate_user!, only: %i[edit_description update_description edit_personal_details update_personal_details]
   before_action :set_user!, only: %i[show]
 
   def show; end
@@ -8,7 +8,7 @@ class MembersController < ApplicationController
   
   def update_description
     respond_to do |format|
-      if current_user.update(user_params)
+      if current_user.update(user_description_params)
         format.turbo_stream do
           render turbo_stream: turbo_stream.replace('member-description',
                                             partial: 'members/member_description',
@@ -18,13 +18,31 @@ class MembersController < ApplicationController
     end
   end
 
+  def edit_personal_details; end
+
+  def update_personal_details
+    respond_to do |format|
+      if current_user.update(user_personal_details_params)
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace('member-personal-details',
+                                            partial: 'members/member_personal_details',
+                                            locals: { user: current_user })
+        end
+      end
+    end
+  end
+  
   private
 
   def set_user!
     @user = User.find(params[:id])
   end
 
-  def user_params
+  def user_description_params
     params.require(:user).permit(:about)
+  end
+
+  def user_personal_details_params
+    params.require(:user).permit(:first_name, :last_name, :city, :state, :country, :pincode, :profile_title)
   end
 end
---
+++ app/javascript/controllers/edit_user_details_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="edit-user-details"
+export default class extends Controller {
+  connect() {
+    console.log("Connected edit-user-details");
+  }
+
+  initialize() {
+    this.element.setAttribute(
+      "data-action",
+      "click->edit-user-details#showModal"
+    );
+  }
+
+  showModal(e) {
+    e.preventDefault();
+    this.url = this.element.getAttribute("href");
+    console.log(this.url);
+    fetch(this.url, {
+      headers: {
+        Accept: "text/vnd.turbo-stream.html",
+      },
+    })
+      .then((resp) => resp.text())
+      .then((html) => Turbo.renderStreamMessage(html))
+      .catch((err) => console.error(err.message));
+  }
+}
