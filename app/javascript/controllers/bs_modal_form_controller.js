59eed0a6d099920dcbdd200807db56982307f0d7
Roman Serediuk <box4rom@gmail.com>
2024-02-01 15:25:39 +0200

    refactor: use general bs-modal-form stimulus controller

--- app/javascript/controllers/bs_modal_form_controller.js
+++ app/javascript/controllers/bs_modal_form_controller.js
@@ -1,14 +1,11 @@
 import { Controller } from "@hotwired/stimulus";
 
-// Connects to data-controller="edit-user-description"
+// Connects to data-controller="bs-modal-form"
 export default class extends Controller {
   connect() {}
 
   initialize() {
-    this.element.setAttribute(
-      "data-action",
-      "click->edit-user-description#showModal"
-    );
+    this.element.setAttribute("data-action", "click->bs-modal-form#showModal");
   }
 
   showModal(e) {
