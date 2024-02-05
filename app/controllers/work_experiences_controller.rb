fff38d798366d9f57f4c1b5aa47e127a87660120
Roman Serediuk <box4rom@gmail.com>
2024-02-05 11:14:40 +0200

    feat: create new work experience

--- app/controllers/work_experiences_controller.rb
+++ app/controllers/work_experiences_controller.rb
@@ -8,7 +8,22 @@ class WorkExperiencesController < ApplicationController
   
   def edit; end
 
-  def create; end
+  def create
+    @work_experience = current_user.work_experiences.new(work_experience_params)
+    respond_to do |format|
+      if @work_experience.save
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.append('work_experience_items',
+          partial: 'work_experiences/work_experience', locals: { work_experience: @work_experience })
+        end
+      else
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace('remote_modal',
+          partial: 'shared/turbo_modal', locals: { form_partial: 'work_experiences/form', modal_title: 'Add new work experience' })
+        end
+      end
+    end
+  end
 
   def update; end
 
@@ -21,7 +36,7 @@ class WorkExperiencesController < ApplicationController
     @work_experience = current_user.work_experiences.find params[:id]
   end
 
-  def work_experience.params
+  def work_experience_params
     params.require(:work_experience)
           .permit(:company, :start_date, :end_date, :user_id,
                   :employment_type, :job_title, :currently_working_here,
