f2ebe6f887342a69106f309133bb3afc25e0d078
Roman Serediuk <box4rom@gmail.com>
2024-02-06 14:52:07 +0200

    use is_same_user?, check_if_already_connected? helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def get_connection_status(current_user, user)
+    current_user.connections.find_by(connected_user_id: user.id ).status
+  end
+
+  def is_same_user?(current_user, user)
+    current_user == user
+  end
 end
