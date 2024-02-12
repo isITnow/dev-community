91b23d0257b91ed30efb74373e60e83afc09b15f
Roman Serediuk <box4rom@gmail.com>
2024-02-12 15:39:03 +0200

    feat: mutual connections

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,17 +1,18 @@
 class MembersController < ApplicationController
   before_action :authenticate_user!, only: %i[edit_description update_description edit_personal_details update_personal_details]
-  before_action :set_user!, only: %i[show]
+  before_action :set_user!, only: %i[show connections]
 
   def show
     @connections = Connection.where('user_id = ? OR connected_user_id = ?',
                                     @user.id, @user.id).where(status: 'accepted')
+    @mutual_connections = current_user.mutually_connected_ids @user
   end
 
   def edit_description; end
   
   def update_description
     respond_to do |format|
-      if current_user.update(user_description_params)
+      if current_user.update user_description_params
         format.turbo_stream do
           render turbo_stream: turbo_stream.replace('member-description',
                                             partial: 'members/member_description',
@@ -25,7 +26,7 @@ class MembersController < ApplicationController
 
   def update_personal_details
     respond_to do |format|
-      if current_user.update(user_personal_details_params)
+      if current_user.update user_personal_details_params
         format.turbo_stream do
           render turbo_stream: turbo_stream.replace('member-personal-details',
                                             partial: 'members/member_personal_details',
@@ -36,17 +37,19 @@ class MembersController < ApplicationController
   end
 
   def connections
-    @requested_connections = Connection.requested_connections(params[:id])
-                                        .where(status: 'accepted')
-    @received_connections = Connection.received_connections(params[:id])
-                                        .where(status: 'accepted')
-    @total_connections = @requested_connections.count + @received_connections.count
+    @connected_users = if params[:mutual_connections].present?
+                          User.where(id: current_user.mutually_connected_ids(@user))
+                        else
+                          User.where(id: @user.connected_user_ids)
+                        end
+    
+    @total_connections = @connected_users.count
   end
   
   private
 
   def set_user!
-    @user = User.find(params[:id])
+    @user = User.find params[:id]
   end
 
   def user_description_params
@@ -56,4 +59,5 @@ class MembersController < ApplicationController
   def user_personal_details_params
     params.require(:user).permit(:first_name, :last_name, :city, :state, :country, :pincode, :profile_title)
   end
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,25 +7,41 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-ActiveRecord::Base.transaction do
-  1000.times do |i|
-    user = User.create(
-      first_name: Faker::Name.first_name,
-      last_name: Faker::Name.last_name,
-      username: Faker::Internet.username(specifier: (5..10), separators: ['_']),
-      email: Faker::Internet.email,
-      contact_number: Faker::PhoneNumber.phone_number_with_country_code,
-      street_address: Faker::Address.street_address,
-      city: Faker::Address.city,
-      state: Faker::Address.state,
-      country: Faker::Address.country,
-      pincode: Faker::Address.postcode,
-      date_of_birth: (Date.today + rand(1..30).days) - rand(24..36).years,
-      profile_title: User::PROFILE_TITLE.sample,
-      password: 'password',
-      about: Faker::Lorem.paragraph(sentence_count: (20..23))
-    )
+# ActiveRecord::Base.transaction do
+#   1000.times do |i|
+#     user = User.create(
+#       first_name: Faker::Name.first_name,
+#       last_name: Faker::Name.last_name,
+#       username: Faker::Internet.username(specifier: (5..10), separators: ['_']),
+#       email: Faker::Internet.email,
+#       contact_number: Faker::PhoneNumber.phone_number_with_country_code,
+#       street_address: Faker::Address.street_address,
+#       city: Faker::Address.city,
+#       state: Faker::Address.state,
+#       country: Faker::Address.country,
+#       pincode: Faker::Address.postcode,
+#       date_of_birth: (Date.today + rand(1..30).days) - rand(24..36).years,
+#       profile_title: User::PROFILE_TITLE.sample,
+#       password: 'password',
+#       about: Faker::Lorem.paragraph(sentence_count: (20..23))
+#     )
 
-    puts "User #{i+1} created successfully"
-  end
-end
+#     puts "User #{i+1} created successfully"
+#   end
+# end
+
+# 10.times do |req|
+#   [201, 1000, 92, 100, 1].each do |rec|
+#     Connection.create(user_id: req + 90, connected_user_id: rec, status: 'accepted')
+#   end
+# end
+
+# ActiveRecord::Base.transaction do
+#   User.find_each do |user|
+#     requested_connections = Connection.requested_connections(user.id).where(status: 'accepted')
+#     received_connections = Connection.received_connections(user.id).where(status: 'accepted')
+#     connected_user_ids = requested_connections.pluck(:connected_user_id) + received_connections.pluck(:user_id)
+
+#     User.where(id: user.id).update_all(connected_user_ids: connected_user_ids)
+#   end
+# end
